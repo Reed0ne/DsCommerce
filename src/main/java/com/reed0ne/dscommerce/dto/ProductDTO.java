7edd5c3bfaf024f97b4c2d31d523ab7ab7d83890
Reed0ne <danyelgianmarquesdasilva@gmail.com>
2024-10-08 11:03:21 -0300

    feat: Bean Validation

--- src/main/java/com/reed0ne/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/reed0ne/dscommerce/dto/ProductDTO.java
@@ -1,11 +1,19 @@
 package com.reed0ne.dscommerce.dto;
 
 import com.reed0ne.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
     private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisa ter entre 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "Preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
