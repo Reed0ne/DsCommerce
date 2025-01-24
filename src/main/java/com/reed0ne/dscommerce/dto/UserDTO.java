e73561f66fc64e4b7b48648a13f09dba87c1ba70
Reed0ne <danyelgianmarquesdasilva@gmail.com>
2025-01-24 18:46:49 -0300

    feat: obtendo usuario logado

---
+++ src/main/java/com/reed0ne/dscommerce/dto/UserDTO.java
@@ -0,0 +1,62 @@
+package com.reed0ne.dscommerce.dto;
+
+import com.reed0ne.dscommerce.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+    private List<String> roles = new ArrayList<>();
+
+    public UserDTO(){}
+
+    public UserDTO(User user) {
+        id = user.getId();
+        name = user.getName();
+        email = user.getEmail();
+        phone = user.getPhone();
+        birthDate = user.getBirthDate();
+        password = user.getPassword();
+
+        for (GrantedAuthority role : user.getRoles()) {
+            roles.add(role.getAuthority());
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+}
