3317b21800f1ea25210f2f2c2a8c18b117aedb38
Reed0ne <danyelgianmarquesdasilva@gmail.com>
2025-02-09 11:49:07 -0300

    fix: validations

--- src/main/java/com/reed0ne/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/reed0ne/dscommerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.reed0ne.dscommerce.dto;
 
 import com.reed0ne.dscommerce.entities.Category;
 import com.reed0ne.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +15,7 @@ public class ProductDTO {
     @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo requerido")
     private String description;
+    @NotNull(message = "Campo requerido")
     @Positive(message = "Preço deve ser positivo")
     private Double price;
     private String imgUrl;
